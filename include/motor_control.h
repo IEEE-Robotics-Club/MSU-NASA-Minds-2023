b8670a183baf9e54f32e28982914c44b99345dc1
liu-bruce <87771104+liu-bruce@users.noreply.github.com>
2022-09-23 08:42:27 -0600

    Add files via upload

---
+++ include/motor_control.h
@@ -0,0 +1,10 @@
+#ifndef motor_control
+#define motor_control
+
+void clockwise_movement(int degrees)
+void counterclock_wise_movement(int degrees)
+void forward_movement(int speed, int time)
+void reverse_movement(int speed, int time)
+
+
+#endif
