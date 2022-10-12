bacff5403c9b4204a8fda0ddfe00fa01a7a4f18f
pepsiman3 <94191657+pepsiman3@users.noreply.github.com>
2022-10-12 13:18:56 -0600

    Improved documentation for vector functions

--- include/math/vec.hpp
+++ include/math/vec.hpp
@@ -1,28 +1,45 @@
-// vec.h
+/**
+ * @file vec.hpp
+ * @author Adam Cate (acate@msudenver.edu) aka pepsiman
+ * @brief Some basic vector types and operations
+ * @version 1.0
+ * @date 2022-03-28
+ * 
+ * @copyright GNU GPL v3.0
+ * 
+ */
 #ifndef VEC_H
 #define VEC_H
 
+/**********************************************************************/
+  /*! @brief Holds a 2d vector, and supports many vector operations like addition and dot product
+   */
+  /**********************************************************************/
 struct Vec2
 {
-    float i = 0, j = 0;
+    float x = 0; //!< x component
+    float y = 0; //!< y component
 
     Vec2();
     Vec2(const Vec2& arg);
     Vec2(float x, float y);
 
-    Vec2& operator=(const Vec2& second);
+    Vec2& operator=(const Vec2& second);        // vector assignment
 
-    Vec2 operator+(const Vec2& second) const;
-    Vec2 operator-(const Vec2& second) const;
+    Vec2 operator+(const Vec2& second) const;   // vector addition
+    Vec2 operator-(const Vec2& second) const;   // vector subtraction
 
     float operator*(const Vec2& second) const;  // dot product
-    Vec2 operator*(const float second) const;
+    Vec2 operator*(const float second) const;   // vector number product
 };
 
-
+/**********************************************************************/
+  /*! @brief Holds a 3d vector, and supports many vector operations like addition and dot product
+   */
+  /**********************************************************************/
 struct Vec3
 {
-    float i = 0, j = 0, k = 0;
+    float x = 0, y = 0, z = 0;
 
     Vec3();
     Vec3(const Vec3& arg);
@@ -38,17 +55,17 @@ struct Vec3
 };
 
 
-float magnitude(Vec2 vec);
-float magnitude(Vec3 vec);
+float magnitude(Vec2 vec);  // calculates the magnitude of a 2d vector
+float magnitude(Vec3 vec);  // calculates the magnitude of a 3d vector
 
 
-Vec3 crossProduct(Vec3 first, Vec3 second);
-Vec3 proj(Vec3 u, Vec3 v);
+Vec3 crossProduct(Vec3 first, Vec3 second); // calculate cross product between two 3d vectors
+Vec3 proj(Vec3 u, Vec3 v);                  // projection of u onto v
 
 
-Vec2 normalize(Vec2 vec);
-Vec3 normalize(Vec3 vec);
+Vec2 normalize(Vec2 vec);                   // normalize a 2d vector
+Vec3 normalize(Vec3 vec);                   // normalize a 3d vector
 
-float AngleFromHorizontal(Vec2 vec);
+float AngleFromHorizontal(Vec2 vec);        // angle from horizontal of a vector
 
 #endif
