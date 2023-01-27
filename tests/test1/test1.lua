93bf66edc6025df4dcfd24b7fa68652bc74cc670
Adam <94191657+pepsiman3@users.noreply.github.com>
2023-01-27 13:02:02 -0700

    basic callback test

--- tests/test1/test1.lua
+++ tests/test1/test1.lua
@@ -1,12 +1,23 @@
 local machine = require('statemachine')
 
 local fsm = machine.create({
-    initial = 'green',
-    events = {
-        { name = 'warn',  from = 'green',  to = 'yellow' },
-        { name = 'panic', from = 'yellow', to = 'red'    },
-        { name = 'calm',  from = 'red',    to = 'yellow' },
-        { name = 'clear', from = 'yellow', to = 'green'  }
-}})
+  initial = 'green',
+  events = {
+    { name = 'warn',  from = 'green',  to = 'yellow' },
+    { name = 'panic', from = 'yellow', to = 'red'    },
+    { name = 'calm',  from = 'red',    to = 'yellow' },
+    { name = 'clear', from = 'yellow', to = 'green'  }
+  },
+  callbacks = {
+    onpanic =  function(self, event, from, to, msg) print('panic! ' .. msg)    end,
+    onclear =  function(self, event, from, to, msg) print('thanks to ' .. msg) end,
+    ongreen =  function(self, event, from, to)      print('green light')       end,
+    onyellow = function(self, event, from, to)      print('yellow light')      end,
+    onred =    function(self, event, from, to)      print('red light')         end,
+  }
+})
 
-print("hello world!")
+fsm:warn()
+fsm:panic('killer bees')
+fsm:calm()
+fsm:clear('sedatives in the honey pots')
