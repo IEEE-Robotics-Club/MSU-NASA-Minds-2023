b5a7118fb5456a4cd1d8e980ed819cbb86089c7f
MitchellThompson1 <98499892+MitchellThompson1@users.noreply.github.com>
2023-02-08 14:58:44 -0700

    fixed middle wheel rotation for each movement function

--- ARGoS-Simulations/lua-only/movement-MVP/movement-functions.lua
+++ ARGoS-Simulations/lua-only/movement-MVP/movement-functions.lua
@@ -1,32 +1,67 @@
+function stopMovement()
+	robot.joints.bl_arm_wheel.set_target(0)
+	robot.joints.ml_arm_wheel.set_target(0)
+	robot.joints.fl_arm_wheel.set_target(0)
+	robot.joints.br_arm_wheel.set_target(0)
+	robot.joints.mr_arm_wheel.set_target(0)
+	robot.joints.fr_arm_wheel.set_target(0)
+end
+
 function driveForward(velocity)
-    robot.joints.base_wheel_bl.set_target(velocity)
-    robot.joints.base_wheel_fl.set_target(velocity)
-    robot.joints.base_wheel_br.set_target(0-velocity)
-    robot.joints.base_wheel_fr.set_target(0-velocity)
+	robot.joints.bl_arm_wheel.set_target(velocity)
+	robot.joints.ml_arm_wheel.set_target(0-velocity)
+	robot.joints.fl_arm_wheel.set_target(velocity)
+	robot.joints.br_arm_wheel.set_target(0-velocity)
+	robot.joints.mr_arm_wheel.set_target(velocity)
+	robot.joints.fr_arm_wheel.set_target(0-velocity)
 end
 
 function turnLeft(velocity)
-    robot.joints.base_wheel_bl.set_target(0)
-    robot.joints.base_wheel_fl.set_target(0)
-    robot.joints.base_wheel_br.set_target(0-velocity)
-    robot.joints.base_wheel_fr.set_target(0-velocity)
+	robot.joints.bl_arm_wheel.set_target(0-velocity)
+	robot.joints.ml_arm_wheel.set_target(velocity)
+	robot.joints.fl_arm_wheel.set_target(0-velocity)
+	robot.joints.br_arm_wheel.set_target(0-velocity)
+	robot.joints.mr_arm_wheel.set_target(velocity)
+	robot.joints.fr_arm_wheel.set_target(0-velocity)
 end
 
 function turnRight(velocity)
-    robot.joints.base_wheel_bl.set_target(velocity)
-    robot.joints.base_wheel_fl.set_target(velocity)
-    robot.joints.base_wheel_br.set_target(0)
-    robot.joints.base_wheel_fr.set_target(0)
+	robot.joints.bl_arm_wheel.set_target(velocity)
+	robot.joints.ml_arm_wheel.set_target(0-velocity)
+	robot.joints.fl_arm_wheel.set_target(velocity)
+	robot.joints.br_arm_wheel.set_target(velocity)
+	robot.joints.mr_arm_wheel.set_target(0-velocity)
+	robot.joints.fr_arm_wheel.set_target(velocity)
 end
 
 function reverse(velocity)
-    robot.joints.base_wheel_bl.set_target(0-velocity)
-    robot.joints.base_wheel_fl.set_target(0-velocity)
-    robot.joints.base_wheel_br.set_target(velocity)
-    robot.joints.base_wheel_fr.set_target(velocity)
+	robot.joints.bl_arm_wheel.set_target(0-velocity)
+	robot.joints.ml_arm_wheel.set_target(velocity)
+	robot.joints.fl_arm_wheel.set_target(0-velocity)
+	robot.joints.br_arm_wheel.set_target(velocity)
+	robot.joints.mr_arm_wheel.set_target(0-velocity)
+	robot.joints.fr_arm_wheel.set_target(velocity)
+end
+
+function getYaw()
+	local w = robot.positioning.orientation.w
+	local x = robot.positioning.orientation.x
+	local y = robot.positioning.orientation.y
+	local z = robot.positioning.orientation.z
+	local t3 = 2*(w*z+x*y)
+	local t4 = 1 - 2*(y*y+z*z)
+
+	local yaw = math.atan(t3, t4)*(180/math.pi)
+	if yaw<0 then
+		yaw = yaw + 360
+	end
+	yaw = yaw * math.pi/180
+	--convert quarternion to z rotation
+	return yaw
 end
 
 function driveTo(x,y, forward_velocity)
+	local isTurning
 	--get x and y of robot
 	local current_x= robot.positioning.position.x
 	local current_y= robot.positioning.position.y
@@ -39,9 +74,8 @@ function driveTo(x,y, forward_velocity)
 	local sum_of_squares=math.pow((x - current_x),2)+math.pow((y - current_y),2)
 
 	local traj_magnitude=math.sqrt(sum_of_squares)	
-
 	--convert quarternion to z rotation
-	local current_angle=math.asin(robot.positioning.orientation.z)*2
+	local current_angle = getYaw()
 
 	--create cartesian face vector 
 
@@ -57,24 +91,30 @@ function driveTo(x,y, forward_velocity)
 	--This angle will always be between 0-180
 	local dot_p = face_vector_cart[1]*traj_vector_cart[1] + face_vector_cart[2]*traj_vector_cart[2]
 	local a = math.deg((math.acos(dot_p/traj_magnitude)))
-	
-	
 
-	log("Angle between :" .. a)
-	log("Cross Product:" .. cross_p)
-	log("Face Angle " .. math.deg(current_angle))
+	--log("Angle between :" .. a)
+	--log("Cross Product:" .. cross_p)
+	--log("Face Angle " .. current_angle)
 	--If the result is negative, Turn Left. Otherwise turn Right. We can use the angle to determine how much we need to turn.
 	local speed_ratio = 10 
 	
-	if cross_p < -0.005 then
+	if cross_p < -0.02 then
 		turnLeft(speed_ratio/2)
+		isTurning = true
 	
-	elseif cross_p > 0.005 then 
+	elseif cross_p > 0.02 then 
 		turnRight(speed_ratio/2)
-	
-	elseif cross_p < 0.005 and cross_p > -0.005 then
-		if a < 2 then
+		isTurning = false
+	else
+		if a < 0.2 then
 				driveForward(forward_velocity)		
 		end
 	end
+	return isTurning
 end
+
+function tablelength(T)
+	local count = 0
+	for _ in pairs(T) do count = count + 1 end
+	return count
+  end
