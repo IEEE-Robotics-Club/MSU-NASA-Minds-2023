d449047b77784e96f06219c77349d6be56359662
Jacob Fifield <jacob.m.fifield@gmail.com>
2023-03-01 08:59:52 -0700

    Fixed issues with lidar setup
    Fixed problems with lidar setup including using radians instead of degrees for the {angle, distance} table.
    
    Also removed tellus-lidar.argos and replaced it with lidar-test.argos

--- ARGoS-Simulations/Tellus-Lidar/lidar-mapping.lua
+++ ARGoS-Simulations/Tellus-Lidar/lidar-mapping.lua
@@ -3,15 +3,15 @@ function lidar_init(velocity)
 end
 
 function lidar_step()
-	local n_rot = math.deg(robot.joints.base_lidar.encoder) 
+	local n_rot = robot.joints.base_lidar.encoder
 	
 	--Since the joints measure total displacement, continually rotating the lidar module will continually increase the lidar encoder position measurement. 
-	if n_rot > 360 then 
-		while n_rot > 360 do 
-			n_rot = n_rot-360.0		
+	if n_rot > 2*math.pi then 
+		while n_rot > 2*math.pi do 
+			n_rot = n_rot-2*math.pi		
 		end
 	end 
 -- Logging for now, but this function should probably return a table containing {distance, phase}
-	return{n_rot, robot.proximity[1],}
+	return{n_rot, math.exp(0.-robot.proximity[1])}
 
 end
