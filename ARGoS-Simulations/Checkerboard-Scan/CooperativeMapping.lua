f6d4231fbdf1817c458e67e3ab7d3106b01b9986
Jacob Fifield <43118015+Fife@users.noreply.github.com>
2023-03-25 17:38:00 -0600

    Update CooperativeMapping.lua

--- ARGoS-Simulations/Checkerboard-Scan/CooperativeMapping.lua
+++ ARGoS-Simulations/Checkerboard-Scan/CooperativeMapping.lua
@@ -125,7 +125,7 @@ function ArrivedAtTarget(map)
 	local current_y= robot.positioning.position.y
 
 	--Create Trajectory vector pointing at the next target 
-	local sum_of_squares = (map["target"][1] - current_x)^2+ ((map["target"][2] - current_y)^2)
+	local sum_of_squares = (map["target"]["x"] - current_x)^2+ ((map["target"]["y"] - current_y)^2)
 	local traj_magnitude = math.sqrt(sum_of_squares)
 
     if (traj_magnitude > 0.1) then
